d267426e86cb872bc11e8a839a160c13cf547294
Sebastian <99194386+Sebastian-FV@users.noreply.github.com>
2022-06-07 20:39:14 -0500

    Crud maven actualizado

--- CRUD MAVEN/Datos.sql
+++ CRUD MAVEN/Datos.sql
@@ -1,12 +1,16 @@
 use BD_Admin_Emple;
 
-#departamento
-insert into Departamentos(idDepartamento,departamento_codigo,departamento_nombre,fecha_hora_crea,fecha_hora_modifica) values ('1','1111','Cundinamarca','2022-03-05  07:05:22' ,'2022-03-05  10:13:24');
+insert into Departamentos values 
+(1, 1111,'Cundinamarca','2022-03-05  07:05:22' ,'2022-03-05  10:13:24'),
+(2, 2222,'Bogotá','2022-05-05  08:05:22' ,'2022-05-05  01:15:30'),
+(3, 3333,'Bogotá','2022-06-08  09:05:22' ,'2022-05-05  01:15:30');
 
-#Empleados
-insert into Empleados(idEmpleado,documento_tipo,documento_numero,nombres,apellidos,departamentos_id,ciudad,dirección,correo_electrónico,Teléfono,fecha_hora_crea,fecha_hora_modifica) value ('1','C.C', '1000602861','Luigui Alexander', 'Rodriguez Dimate','1', 'Bogota', 'calle 50 # 62 f 77', 'alex.lui@gmail.com','3204429180','2022-02-10  08:11:24','2022-05-02 11:31:11'); 
+insert into Empleados value 
+(1,'C.C', 1000602861,'Luigui Alexander', 'Rodriguez Dimate','1', 'Bogota', 'calle 50 # 62 f 77', 'alex.lui@gmail.com','3204429180','2022-02-11  08:10:24','2022-05-09 05:25:01'),
+(2,'C.C', 1000602971,'Gorgy Alejandro', 'Casallas Bernal','2', 'Bogota', 'calle 45 # 84 f 88', 'gorgy.Alej@gmail.com','3204421291','2022-03-12  09:11:24','2022-05-08 01:45:02'),
+(3,'C.C', 1000266355,'Sebastian', 'Rodriguez Dimate','3', 'Bogota', 'calle 30 # 73 f 99', 'Sebas.Fore@gmail.com','3204432312','2022-04-13  09:12:24','2022-05-07 12:15:03'); 
 
-#ciudad 
-insert into Ciudad(nombre_ciudad,codigo_cuidad) values ('Bogota','1');
-
-select*from Empleados;
+insert into Ciudad values 
+('Bogota', 1),
+('Bogota', 2),
+('Bogota', 3);
